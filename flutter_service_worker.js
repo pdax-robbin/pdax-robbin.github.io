ffe3122461a37582b7c5c01d86ca4d6a976aea7c
Robbin Ramos <robbin.ramos@pdax.ph>
2021-05-03 10:36:09 +0800

    somechanges

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "5383add18283faaa035af1511f083396",
-"index.html": "2651c6665ef8a3723e3bbc1be115697a",
-"/": "2651c6665ef8a3723e3bbc1be115697a",
-"main.dart.js": "12b23ce461cf4465343b13bb54672dfc",
+"index.html": "124ed6a706971766699ae0044a341d06",
+"/": "124ed6a706971766699ae0044a341d06",
+"main.dart.js": "4cf3059b3ed5d08fd1ff9b668add436e",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
